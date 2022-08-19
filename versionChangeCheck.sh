03f24dc75b6a8cd3df2c6d0d611a3d8708f3020a
Michael Dryden <mjdryden@gmail.com>
2022-08-19 16:33:33 -0500

    Release CI
    Progress

--- versionChangeCheck.sh
+++ versionChangeCheck.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-changes="$(git diff HEAD~1 HEAD setup.py | grep version=)"
+changes="$(git diff HEAD~1 HEAD pyproject.toml | grep version=)"
 
 # echo "[ $changes ]"
 if [ "$changes" != "" ]; then
-    version="$(cat setup.py | grep "version=" | cut -d"=" -f 2 | cut -d "\"" -f 2)"
+    version="$(cat pyproject.toml | grep "version =" | cut -d"=" -f 2 | cut -d "\"" -f 2)"
     echo $version
 fi
